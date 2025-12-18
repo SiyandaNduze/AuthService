81d5cc573a82198e06320afe322804d5356e2537
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-18 21:14:40 +0200

    added a controller

---
+++ Auth.API/Controllers/SecureController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/secure")]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+            => Ok("You are authenticated");
+    }
+
+}
