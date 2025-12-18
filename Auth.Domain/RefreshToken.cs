222e5f4652c749b5b458766e413d212f356ca600
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-18 19:34:28 +0200

    added a refresh token to auth.domain

---
+++ Auth.Domain/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Auth.Domain
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Token { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
