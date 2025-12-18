25d1782441650fdc673b4aad8542fe670ecbb3a6
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-18 17:40:25 +0200

    added 2 classes in auth.infrustructer callin itokenservice and ipasswordhasher with the necessary logic

---
+++ Auth.Infrastructure/JwtTokenService.cs
@@ -0,0 +1,18 @@
+﻿using Auth.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+
+public class JwtTokenService : ITokenService
+{
+    private readonly IConfiguration _config;
+
+    public JwtTokenService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public AuthResult GenerateTokens(User user)
+    {
+        // Generate access + refresh tokens
+        return new AuthResult("access-token", "refresh-token");
+    }
+}
