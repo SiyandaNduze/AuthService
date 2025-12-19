e6474b2c77136c4fa27c1139b7bf1555ef8f8710
Siyandaaa <ST10440706@vcconnect.edu.za>
2025-12-19 16:30:33 +0200

    minor dedugging

--- Auth.API/Controllers/SecureController.cs
+++ Auth.API/Controllers/SecureController.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Auth.API.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("api/secure")]
     public class SecureController : ControllerBase
     {
+        [Authorize]
         [HttpGet]
         public IActionResult Get()
             => Ok("You are authenticated");
